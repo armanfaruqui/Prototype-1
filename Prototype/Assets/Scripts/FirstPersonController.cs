26c6cb530f115a3e4ffa3c2c0f5eb1a44643e26a
armanfaruqui <70916901+armanfaruqui@users.noreply.github.com>
2022-02-02 11:34:53 -0500

    Added player movement

---
+++ Prototype/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public float mouseSensitivityX = 250f; // Horizontal sens
+    public float mouseSensitivityY = 250f; // Vertical sens
+
+    public float walkSpeed = 8.0f; 
+    public float jumpForce = 220;
+
+    public LayerMask groundedMask;
+    Transform cameraT;
+
+    float verticalLookRotation; // Stores the amount the player is rotated horizontally
+
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+
+    bool grounded; // True is player is grounded
+
+    public float smoothAmount = 0.15f;
+    public float verticalConstraint = 60f; // Stores the degrees at which the player is able to look up or down
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraT = Camera.main.transform; // Finds the main camera
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivityX); // Takes vertical mouse movement. Rotates the camera
+        verticalLookRotation += Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivityY; // Takes horizontal mouse movement
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -verticalConstraint, verticalConstraint); // Prevents the camera from rotating higher or lower than the contrains
+        cameraT.localEulerAngles = Vector3.left * verticalLookRotation; //  Rotates the player capsule
+
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized; // Stores vector for direction the player should be moving in
+        Vector3 targetMoveAmount = moveDir * walkSpeed; // Controlls the speed in that direction
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, smoothAmount); // Smoothes the movement between axises
+
+        if (Input.GetButtonDown("Jump")){
+            if (grounded){
+            GetComponent<Rigidbody>().AddForce(transform.up * jumpForce);
+          }
+        }
+        grounded = false;
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1 + 0.1f, groundedMask)){
+            grounded = true;
+        }
+
+    }
+
+    void FixedUpdate(){
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime); // Updates the player's position
+    }
+}
